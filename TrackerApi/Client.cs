ac0a51fa1ddd710811ceed5f0c4e58f55210f08a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-18 18:03:05 +0100

    Add TrackerApi
    Change-Id: I062b6dbfaff79b17c1bc405dba24eebfcf868610

---
+++ TrackerApi/Client.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TrackerApi.JsonModels;
+
+namespace TrackerApi
+{
+    //Constructors
+    public partial class TrackerClient
+    {
+        private readonly HttpClient _http;
+        public TrackerClient()
+        {
+            _http = new HttpClient();
+            _http.BaseAddress = new Uri("https://tracker.wikimedia.cz/api/");
+        }
+    }
+    //GET
+    public partial class TrackerClient
+    {
+        public async Task<Mediainfo> GetMediainfo(int id)
+        {
+            var resp = await _http.GetAsync($"tracker/mediainfo/{id}");
+            string respString = await resp.Content.ReadAsStringAsync();
+            return JObject.Parse(respString).ToObject<Mediainfo>();
+        }
+    }
+
+}
---
+++ TrackerApi/JsonModels/Mediainfo.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerApi.JsonModels
+{
+    public class Mediainfo
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("descriptionurl")]
+        public string DescriptionUrl { get; set; }
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("width")]
+        public int Width { get; set; }
+        [JsonProperty("height")]
+        public string Height { get; set; }
+        [JsonProperty("thumb_url")]
+        public string ThumbUrl { get; set; }
+        [JsonProperty("canonicaltitle")]
+        public string CanonicalTitle { get; set; }
+        [JsonProperty("ticket")]
+        public string Ticket { get; set; }
+        /*[JsonProperty("categories")]
+        public List<string> Categories { get; set; }*/
+    }
+}
