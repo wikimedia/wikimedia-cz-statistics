3630326e4f013ad562764beb3c7945c3c56e2711
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-18 18:04:06 +0100

    Add TestController
    Change-Id: I620da3ad2f5133fa2fb6965620e2dbb7ebdac2b5

---
+++ statistics.Server/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrackerApi;
+using TrackerApi.JsonModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace statistics.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class TestController : Controller
+    {
+        TrackerClient tc;
+        public TestController()
+        {
+            tc = new TrackerClient();
+        }
+        [HttpGet]
+        public async Task<Mediainfo> TohleJeSubNeco()
+        {
+            return await tc.GetMediainfo(9236);
+        }
+    }
+}
