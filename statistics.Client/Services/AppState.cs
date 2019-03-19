0a147f32b0eaf7af2c947943f8a4e14dcea19c79
Tomas Pelak <info@pelaktomas.cz>
2019-03-19 12:53:56 +0100

    add State managment for blazor

---
+++ statistics.Client/Services/AppState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace statistics.Client.Services
+{
+    public class AppState
+    {
+        private readonly AppClient _client;
+        public AppState(AppClient client)
+        {
+            _client = client;
+        }
+    }
+}
