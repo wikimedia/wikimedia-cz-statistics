d8fedce625c262f50a75ab8f27b26c21e7682803
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-19 11:25:36 +0100

    [test] Fill TestController with testing API
    Change-Id: I7bc663d911e072b13bf8f286c1e1343691ad5421

--- statistics.Server/Controllers/TestController.cs
+++ statistics.Server/Controllers/TestController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TrackerApi;
 using TrackerApi.JsonModels;
+using WikiClientLibrary.Client;
+using WikiClientLibrary.Generators;
+using WikiClientLibrary.Pages;
+using WikiClientLibrary.Sites;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,14 +18,57 @@ namespace statistics.Server.Controllers
     public class TestController : Controller
     {
         TrackerClient tc;
+        WikiClient wc;
+        WikiSite cswiki;
+        private async Task<WikiSite> InitWiki(string url)
+        {
+            WikiSite s = new WikiSite(wc, url);
+            await s.Initialization;
+            return s;
+        }
         public TestController()
         {
+            // Init Tracker communication
             tc = new TrackerClient();
+
+            // Init wiki communication
+            wc = new WikiClient
+            {
+                ClientUserAgent = "Urbanecm's testing webapp (urbanecm@tools.wmflabs.org)"
+            };
+            cswiki = InitWiki("https://cs.wikipedia.org/w/api.php").Result;
         }
-        [HttpGet]
         public async Task<Mediainfo> TohleJeSubNeco()
         {
             return await tc.GetMediainfo(9236);
+        }
+
+        public async Task<IEnumerable<WikiPage>> MediawikiStrangeResult()
+        {
+            var generator = new AllPagesGenerator(cswiki)
+            {
+                StartTitle = "Mar"
+            };
+            var pages = await generator.EnumPagesAsync().Take(10).ToList();
+            foreach (var page in pages)
+            {
+                page.Content = "";
+            }
+            return pages;
+        }
+
+        public async Task<IEnumerable<string>> Mediawiki()
+        {
+            var generator = new AllPagesGenerator(cswiki)
+            {
+                StartTitle = "Mar"
+            };
+            var pages = await generator.EnumPagesAsync().Take(10).ToList();
+            foreach (var page in pages)
+            {
+                page.Content = "";
+            }
+            return pages.Select(p => p.Title);
         }
     }
 }
