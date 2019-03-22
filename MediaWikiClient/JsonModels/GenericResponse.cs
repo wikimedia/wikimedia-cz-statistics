46696b0ce775244acfe5bdd5bac3e4e022a4c86d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-22 14:35:00 +0100

    Deserialize page with much nicer way
    Change-Id: I078b3d2e5b4c976eb7e24e2f728d59afddb31b75

--- MediaWikiClient/JsonModels/GenericResponse.cs
+++ MediaWikiClient/JsonModels/GenericResponse.cs
@@ -2,47 +2,27 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Threading;
 
 namespace MediaWikiClient.JsonModels
 {
     public class GenericResponse
     {
-        private Dictionary<string, Page> pages;
-
         [JsonProperty("batchcomplete")]
         public string Batchcomplete { get; set; }
         [JsonProperty("continue")]
         public Dictionary<string, string> Continue { get; set; }
         [JsonProperty("query")]
         public Dictionary<string, object> Query { get; set; }
-        public Dictionary<string, Page> Pages {
-            get
-            {
-                while(pages == null)
-                {
-                    Thread.Sleep(10);
-                }
-                return pages;
-            }
-            set
-            {
-                pages = value;
-            }
-        }
+        public Dictionary<string, Page> Pages { get; set; }
         [JsonProperty("Limits")]
         public Dictionary<string, int> Limits { get; set; }
 
-        public GenericResponse()
+        [OnDeserialized]
+        public void DeserializeQuery(StreamingContext context)
         {
-            new Thread(() =>
-            {
-                while (Query == null)
-                {
-                    Thread.Sleep(10);
-                }
-                Pages = ((JObject)Query["pages"]).ToObject<Dictionary<string, Page>>();
-            }).Start();
+            Pages = ((JObject)Query["pages"]).ToObject<Dictionary<string, Page>>();
         }
     }
 }
