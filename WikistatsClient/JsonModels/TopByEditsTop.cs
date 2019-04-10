1ab83c23708a24c4c8cf4ae9c4dde685767fbc6c
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-04-10 14:26:07 +0200

    Finish content of TopByEditsTop
    Change-Id: Ib9ec56752e3d8abdf61d20c01578181ab0fcc66c

--- WikistatsClient/JsonModels/TopByEditsTop.cs
+++ WikistatsClient/JsonModels/TopByEditsTop.cs
@@ -1,6 +1,14 @@
-﻿namespace WikistatsClient.JsonModels
+﻿using Newtonsoft.Json;
+
+namespace WikistatsClient.JsonModels
 {
     public class TopByEditsTop
     {
+        [JsonProperty("page_title")]
+        public string PageTitle { get; set; }
+        [JsonProperty("edits")]
+        public int Edits { get; set; }
+        [JsonProperty("rank")]
+        public int Rank { get; set; }
     }
 }
